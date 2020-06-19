bc1e79d467e2bf059659b017d17b753ff85505d0
89luca89 <luca.dimaio1@gmail.com>
2020-06-19 10:48:36 +0200

    fix typo

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ case "$HOST" in
     "local")
         ansible-playbook -i localhost, -c local -K main.yml --skip-tags reboot $@
          ;;
-    "*")
+    *)
         ansible-playbook -i $HOST, -kK main.yml $@
         ;;
 esac
