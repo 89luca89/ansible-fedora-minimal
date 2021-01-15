a6973826ce8653a371a8f92843af9bb1adf10d03
89luca89 <luca.dimaio1@gmail.com>
2021-01-15 13:16:18 +0100

    Make the install script work outside of folder

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+
+DIR="$(dirname $0)"
+
 HOST=$1
 shift
 
 case "$HOST" in
     "local")
-        ansible-playbook -i localhost, -c local -K main.yml --skip-tags reboot $@
+        ansible-playbook -i localhost, -c local -K "$DIR"/main.yml --diff --skip-tags reboot $@
          ;;
     *)
-        ansible-playbook -i $HOST, -kK main.yml $@
+        ansible-playbook -i $HOST, -kK "$DIR"/main.yml --diff $@
         ;;
 esac
