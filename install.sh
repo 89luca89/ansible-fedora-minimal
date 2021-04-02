9f50c54a156881e6048e99530810c3c98d0c89b7
89luca89 <luca.dimaio1@gmail.com>
2021-04-02 08:44:11 +0200

    Disable hostkeycheck in install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ DIR="$(dirname $0)"
 HOST=$1
 shift
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
 case "$HOST" in
     "local")
         ansible-playbook -i localhost, -c local -K "$DIR"/main.yml --diff --skip-tags reboot $@
